cf0615ac994f16e28e6b6dd1dbc432874567fd8e
requireCool <1019715750@qq.com>
2021-10-11 10:14:14 +0000

    try

--- QuantumultX/Dianshijia/dianshijia.js
+++ QuantumultX/Dianshijia/dianshijia.js
@@ -76,7 +76,7 @@ if (isGetCookie = typeof $request !== 'undefined') {
     bjTime = new Date(timestamp).toLocaleString('zh', {hour12: false,timeZoneName: 'long'}); //标准北京时间
     console.log(`\n === 脚本执行 ${bjTime} ===\n`);
     console.log(`------------- 共${tokenArr.length}个账号`);
-   
+    console.log(`rewardID=${RewardId}`)
     for (let i = 0; i < tokenArr.length; i++) {
         if (tokenArr[i]) {
             signheaderVal = tokenArr[i];
