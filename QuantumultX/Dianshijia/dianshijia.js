2b7de37d1d3d9b70138e7bb90b3b267522a5da80
requireCool <1019715750@qq.com>
2021-10-31 05:40:26 +0000

    .

--- QuantumultX/Dianshijia/dianshijia.js
+++ QuantumultX/Dianshijia/dianshijia.js
@@ -94,8 +94,8 @@ if (isGetCookie = typeof $request !== 'undefined') {
             await Addsign(); // 额外奖励，默认额度
             await run();
             await tasks(); // 任务状态 *
-            //await getGametime(); // 游戏时长
-            await total(); // 总计
+            await getGametime(); // 游戏时长
+            //await total(); // 总计
             await cash(); // 现金
             await cashlist(); // 现金列表 *
             await coinlist(); // 金币列表
